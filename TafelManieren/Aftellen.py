29d6f5d01483ef67ab41b7db87bb4c692acb0f0f
HalfVarious <99067555@mydavinci.nl>
2021-10-26 09:56:11 +0200

    Tafelmanieren

---
+++ TafelManieren/Aftellen.py
@@ -0,0 +1,8 @@
+import time
+
+for x in range(30, 0, -1):
+    print(x)
+
+    time.sleep(1)
+
+print("Lift-off")   
---
+++ TafelManieren/Uren.py
@@ -0,0 +1,5 @@
+
+for t in range(1, 13 ):
+    print(str(t) + "AM")
+for i in range(1, 13):
+    print(str(i) + "PM")
